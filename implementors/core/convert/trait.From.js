4cb683a0be894f625fe46530196c09fe68241c27
Travis CI User <travis@example.org>
2018-03-20 02:49:48 +0000

    Update documentation

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["openvr"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"openvr_sys/struct.TrackedDevicePose_t.html\" title=\"struct openvr_sys::TrackedDevicePose_t\">TrackedDevicePose_t</a>&gt; for <a class=\"struct\" href=\"openvr/struct.TrackedDevicePose.html\" title=\"struct openvr::TrackedDevicePose\">TrackedDevicePose</a>",synthetic:false,types:["openvr::tracking::TrackedDevicePose"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"openvr_sys/struct.VREvent_t.html\" title=\"struct openvr_sys::VREvent_t\">VREvent_t</a>&gt; for <a class=\"struct\" href=\"openvr/system/event/struct.EventInfo.html\" title=\"struct openvr::system::event::EventInfo\">EventInfo</a>",synthetic:false,types:["openvr::system::event::EventInfo"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
